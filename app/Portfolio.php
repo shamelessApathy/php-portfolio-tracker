18f447a446a11aa15833cb9624234e296ae52e09
Brian Moniz <brian@prohoods.com>
2017-08-06 08:24:24 -0600

    Added relations in the models

--- app/Portfolio.php
+++ app/Portfolio.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Portfolio extends Model
 {
     //
+    public function transactions()
+    {
+    	return $this->hasMany('App\Transaction');
+    }
+    public function user()
+    {
+    	return $this->belongsTo('App\User');
+    }
 }
--- app/Transaction.php
+++ app/Transaction.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     //
+    public function portfolio()
+    {
+    	return $this->belongsTo('App\Portfolio');
+    }
 }
