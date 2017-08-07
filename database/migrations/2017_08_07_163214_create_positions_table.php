2a51f3e58951c24ae80543e04234f4a3b834d3e8
B <bcm811@gmail.com>
2017-08-07 10:42:19 -0600

    added controllers and migrations for positions and transactions

---
+++ database/migrations/2017_08_07_163214_create_positions_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePositionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('positions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('quantity');
+            $table->enum('type',['short','long']);
+            // Portfolio Foreign Key
+            $table->integer('portfolio_id')->unsigned();
+
+            $table->foreign('portfolio_id')->references('id')->on('portfolios');
+            // Users Foreign Key
+            $table->integer('user_id')->unsigned();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            // Stocks Foreign Key
+            $table->integer('stock_id')->unsigned();
+
+            $table->foreign('stock_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
