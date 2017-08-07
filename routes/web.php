4700e8161c542152e3e777bfd5fbeea782d1562e
B <bcm811@gmail.com>
2017-08-07 14:12:07 -0600

    after etrade init commit

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,33 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+#File: app/Http/routes.php
+Route::get('/show-autoloaders', function(){
+    foreach(spl_autoload_functions() as $callback)
+    {
+        if(is_string($callback))
+        {
+            echo '- ',$callback,"\n<br>\n";
+        }
 
+        else if(is_array($callback))
+        {
+            if(is_object($callback[0]))
+            {
+                echo '- ',get_class($callback[0]);
+            }
+            elseif(is_string($callback[0]))
+            {
+                echo '- ',$callback[0];
+            }
+            echo '::',$callback[1],"\n<br>\n";            
+        }
+        else
+        {
+            var_dump($callback);
+        }
+    }
+});
 Route::get('/', function () {
     return view('welcome');
 });
