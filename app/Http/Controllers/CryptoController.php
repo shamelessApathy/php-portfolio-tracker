51d4d748b9e0d758d6a94a1c65e5dadc8cbeee53
B <bcm811@gmail.com>
2017-08-23 15:40:48 -0600

    added simple api module for dwarfpool API

--- app/Http/Controllers/CryptoController.php
+++ app/Http/Controllers/CryptoController.php
@@ -6,14 +6,15 @@ use Illuminate\Http\Request;
 
 class CryptoController extends Controller
 {
-	private $moneroWallet = "46ThMeiYtpyStR2wM642A52U5xy6VPUiqEWPZyGwEiuTXshbcbWLU9aJxXf7gTJLkGgAxZfG9efkGcmbHmo3dUG9G2mKLxV";
+	
     // Gets Mining Info for each currency
     public function getMinerInfo()
     {
     	$ether = $this->getEthereumInfo();
     	$zcash = $this->getZcashInfo();
-
-    	$minerinfo = array('ethereum'=>$ether,'zcash'=>$zcash);
+    	$monero = $this->getMoneroInfo();
+    	$current_prices = $this->getCurrentPrices();
+    	$minerinfo = array('ethereum'=>$ether,'zcash'=>$zcash,'monero'=>$monero,'current_prices'=>$current_prices);
     	return $minerinfo;
     }
     public function getEthereumInfo()
@@ -22,7 +23,7 @@ class CryptoController extends Controller
     	$url = "http://dwarfpool.com/eth/api?wallet=".$ethereumWallet."&email=bcm811@gmail.com";
     	// Make request (seems rather simple)
     	$response = file_get_contents($url);
-    	$decoded = json_decode($response);
+    	$decoded = json_decode($response,2);
     	return $decoded;
     }
     public function getZcashInfo()
@@ -32,7 +33,26 @@ class CryptoController extends Controller
     	// Make request
     	$response = file_get_contents($url);
     	// Decode JSON
-    	$decoded = json_decode($response);
+    	$decoded = json_decode($response,2);
     	return $decoded;
+    }
+    public function getMoneroInfo()
+    {
+    	$moneroWallet = "46ThMeiYtpyStR2wM642A52U5xy6VPUiqEWPZyGwEiuTXshbcbWLU9aJxXf7gTJLkGgAxZfG9efkGcmbHmo3dUG9G2mKLxV";
+    	$url = "http://dwarfpool.com/xmr/api?wallet=".$moneroWallet."&email=bcm811@gmail.com";
+
+    	// Make request
+    	$response = file_get_contents($url);
+    	// json_decode($data, FLAG) 1 = StdClass Object    2 = Array
+    	$decoded = json_decode($response, 2);
+    	return $decoded;
+    }
+    public function getCurrentPrices()
+    {
+    	$ethereum_price = file_get_contents("https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,EUR");
+    	$zcash_price = file_get_contents("https://min-api.cryptocompare.com/data/price?fsym=ZEC&tsyms=BTC,USD,EUR");
+    	$monero_price = file_get_contents("https://min-api.cryptocompare.com/data/price?fsym=XMR&tsyms=BTC,USD,EUR");
+    	$price_data = array('ethereum_price'=>$ethereum_price,'zcash_price'=>$zcash_price,'monero_price'=>$monero_price);
+    	return $price_data;
     }
 }
--- resources/views/crypto/dashboard.blade.php
+++ resources/views/crypto/dashboard.blade.php
@@ -1,6 +1,96 @@
+<?php 
+$ethereum = $minerinfo['ethereum'];
+$zcash = $minerinfo['zcash'];
+$monero = $minerinfo['monero'];
+$current_prices = $minerinfo['current_prices'];
+?>
 <div class='crypto-dash-container'>
-<ul>
-	<li>Ethereum</li>
-	<li>Zcash</li>
-</ul>
+<details>
+	<summary>Ethereum <?php echo $ethereum['total_hashrate'];?> MH/s</summary>
+	<details>
+	<summary>Workers <?php echo count($ethereum['workers']);?></summary>
+	<?php foreach($ethereum['workers'] as $worker):?>
+		<ul>
+		<?php echo (empty($worker['worker'])  ? "SimpleMiner" : $worker['worker']) ?>
+		<li><?php echo $worker['hashrate'];?>  --actual</li>
+		<li><?php echo $worker['hashrate_calculated'];?>   --calculated</li>
+		<li>Seconds since last share: <?php echo $worker['second_since_submit'];?></li>
+		</ul>
+	<?php endforeach;?>
+	</details>
+	<details>
+	<summary>More Info</summary>
+	<ul>
+		<li>Autopayout: <?php echo $ethereum['autopayout_from'];?></li>
+		<li>Last Payment: <?php echo $ethereum['last_payment_amount'];?></li>
+		<li>Payment Date: <?php echo $ethereum['last_payment_date'];?></li>
+		<li>Wallet: <?php echo $ethereum['wallet'];?></li>
+		<li>Pool Balance: <?php echo $ethereum['wallet_balance'];?></li>
+	</ul>
+	</details>
+</details>
+<details>
+	<summary>Zcash <?php echo $zcash['total_hashrate'];?> sols</summary>
+	<details>
+	<summary>Workers <?php echo count($zcash['workers']);?></summary>
+	<?php foreach($zcash['workers'] as $worker):?>
+		<ul>
+		<?php echo (empty($worker['worker'])  ? "SimpleMiner" : $worker['worker']) ?>
+		<li><?php echo $worker['hashrate'];?>  --actual</li>
+		<li><?php echo $worker['hashrate_calculated'];?>   --calculated</li>
+		<li>Seconds since last share: <?php echo $worker['second_since_submit'];?></li>
+		</ul>
+	<?php endforeach;?>
+	</details>
+	<details>
+	<summary>More Info</summary>
+	<ul>
+		<li>Autopayout: <?php echo $zcash['autopayout_from'];?></li>
+		<li>Last Payment: <?php echo $zcash['last_payment_amount'];?></li>
+		<li>Payment Date: <?php echo $zcash['last_payment_date'];?></li>
+		<li>Wallet: <?php echo $zcash['wallet'];?></li>
+		<li>Pool Balance: <?php echo $zcash['wallet_balance'];?></li>
+	</ul>
+	</details>
+</details>
+<details>
+	<summary>Monero <?php echo $monero['total_hashrate_calculated'];?> kH/s</summary>
+	<details>
+	<summary>Workers <?php echo count($monero['workers']);?></summary>
+	<?php foreach($monero['workers'] as $worker):?>
+		<ul>
+		<?php echo (empty($worker['worker'])  ? "SimpleMiner" : $worker['worker']) ?>
+		<li><?php echo $worker['hashrate'];?>  --actual</li>
+		<li><?php echo $worker['hashrate_calculated'];?>   --calculated</li>
+		<li>Seconds since last share: <?php echo $worker['second_since_submit'];?></li>
+		</ul>
+	<?php endforeach;?>
+	</details>
+	<details>
+	<summary>More Info</summary>
+	<ul>
+		<li>Autopayout: <?php echo $monero['autopayout_from'];?></li>
+		<li>Last Payment: <?php echo $monero['last_payment_amount'];?></li>
+		<li>Payment Date: <?php echo $monero['last_payment_date'];?></li>
+		<li>Wallet: <?php echo $monero['wallet'];?></li>
+		<li>Pool Balance: <?php echo $monero['wallet_balance'];?></li>
+	</ul>
+	</details>
+</details>
 </div>
+	<?php 
+		echo "<pre>";
+		print_r($current_prices);
+		echo "</pre>";
+
+		/*$count = 0;
+		foreach ($minerinfo['ethereum']->workers as $worker)
+		{
+			$count++;
+		}
+		var_dump($count);*/
+		//$array = get_object_vars($minerinfo['ethereum']->workers);
+		//$count = count($array);
+		//var_dump($count);
+
+	?>
